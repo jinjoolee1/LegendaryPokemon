e1fc565ed209b1efa81b6aba57595acd9c51c052
jinjoolee1 <jinjoolee126@gmail.com>
2023-08-01 16:54:50 -0500

    pokemon api issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
+
 function App() {
+
+  const [pokemonName, setPokemonName] = useState("");
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response) => {
+    
+    console.log(response);
+    }
+  );
+};
+
   return (
     <div className="App">
       <div className="TitleSection">
       <h1>Legendary & Mythical Pokémon </h1>
-      <input type="text" />
-      <button>Search Pokémon</button>
+      <input type="text" onChange={(event) => {
+        setPokemonName(event.target.value);
+      }}
+      />
+      <button onClick={searchPokemon}>Search Pokémon</button>
       </div>
     </div>
   );
