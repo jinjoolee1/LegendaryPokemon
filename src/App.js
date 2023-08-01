f057c4471888498eedf0399019cea7bb9a45ec54
jinjoolee1 <jinjoolee126@gmail.com>
2023-08-01 16:21:07 -0500

    initial push and some editing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div className="TitleSection">
+      <h1>Legendary & Mythical Pokémon </h1>
+      <input type="text" />
+      <button>Search Pokémon</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
